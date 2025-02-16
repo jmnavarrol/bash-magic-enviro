e085e2ab035654ae581fa327f80507797536c43b
Jesús M. Navarro <jesusmnavarrolopez@gmail.com>
2025-02-16 12:33:16 +0100

    #12: macos support: OS checking refactored.

--- make-checks.sh
+++ make-checks.sh
@@ -52,14 +52,14 @@ check_destdir_in_path() {
 check_os() {
 	case "$OSTYPE" in
 		darwin*)
-			local os_msg="MacOS detected ($OSTYPE)\n"
-			os_msg+="\tchecking for GNU tooling..."
+			local os_msg="${C_BOLD}* MacOS detected ($OSTYPE)${C_NC}. "
+			os_msg+="Checking GNU tooling..."
 			echo -e "${os_msg}"
 			check_macos || return $?
 		;;
 		linux*)
-			local os_msg="Linux detected ($OSTYPE)\n"
-			os_msg+="\tFollow on."
+			local os_msg="${C_BOLD}* Linux detected ($OSTYPE)${C_NC}. "
+			os_msg+="Follow on."
 			echo -e "${os_msg}"
 		;;
 		*)
@@ -78,90 +78,42 @@ check_os() {
 
 # Custom checks on macOS
 check_macos() {
-	mkdir --parents .testdir > /dev/null 2>&1 || {
-		local err_rc=$?
-	# trying coreutils' "g-versions" on macos
-		if ! which gmkdir > /dev/null; then
-			local err_msg="${C_RED}ERROR:${C_NC} while testing ${C_BOLD}'$OSTYPE'${C_NC}:\n"
-			err_msg+="\tGNU tooling couldn't be found.\n"
-			err_msg+="\tYou should install ${C_BOLD}'brew install coreutils'${C_NC} and follow instructions to properly set '\$PATH' environment variable."
+local -A gnu_tools=(
+	['mkdir']='coreutils'
+	['find']='findutils'
+	['sed']='gnu-sed'
+)
+# common tools
+	for gnu_tool in ${!gnu_tools[@]}; do
+		${gnu_tool} --version > /dev/null 2>&1 || {
+		# trying tool's "g-version" on macos
+			if ! which g${gnu_tool} > /dev/null; then
+				local err_msg="${C_RED}ERROR:${C_NC} while testing ${C_BOLD}'$OSTYPE'${C_NC}:\n"
+				err_msg+="\tGNU tooling couldn't be found.\n"
+				err_msg+="\tYou should install ${C_BOLD}'brew install ${gnu_tools[${gnu_tool}]}'${C_NC} and follow instructions to properly set ${C_BOLD}'\$PATH'${C_NC} environment variable."
+			else
+				local err_msg="${C_RED}ERROR:${C_NC} while testing ${C_BOLD}'$OSTYPE'${C_NC}:\n"
+				err_msg+="\tit seems ${C_BOLD}'brew ${gnu_tools[${gnu_tool}]}'${C_NC} is installed but unconfigured.\n"
+				err_msg+="\tyou need to propely set your ${C_BOLD}'\$PATH'${C_NC} environment variable."
+			fi
 			echo -e "${err_msg}"
 			error_dependencies=true
-		# non-recoverable error: exit immediately
-			exit $err_rc
-		else
-			local err_msg="${C_RED}ERROR:${C_NC} while testing ${C_BOLD}'$OSTYPE'${C_NC}:\n"
-			err_msg+="\tit seems ${C_BOLD}'brew coreutils'${C_NC} is installed but unconfigured.\n"
-			err_msg+="\tyou need to propely set your '\$PATH' environment variable."
-			echo -e "${err_msg}"
-			error_dependencies=true
-		# non-recoverable error: exit immediately
-			exit $err_rc
-		fi
-	}
-# other gnu tools
-	# find
-	find --version > /dev/null 2>&1 || {
-	# trying findutils' "g-version" on macos
-		if ! which gfind > /dev/null; then
-			local err_msg="${C_RED}ERROR:${C_NC} while testing ${C_BOLD}'$OSTYPE'${C_NC}:\n"
-			err_msg+="\tGNU tooling couldn't be found.\n"
-			err_msg+="\tYou should install ${C_BOLD}'brew install findutils'${C_NC} and follow instructions to properly set '\$PATH' environment variable."
-			echo -e "${err_msg}"
-			error_dependencies=true
-		# non-recoverable error: exit immediately
-			exit $err_rc
-		else
-			local err_msg="${C_RED}ERROR:${C_NC} while testing ${C_BOLD}'$OSTYPE'${C_NC}:\n"
-			err_msg+="\tit seems ${C_BOLD}'brew findutils'${C_NC} is installed but unconfigured.\n"
-			err_msg+="\tyou need to propely set your '\$PATH' environment variable."
-			echo -e "${err_msg}"
-			error_dependencies=true
-		# non-recoverable error: exit immediately
-			exit $err_rc
-		fi
-	}
-	# sed
-	sed --version > /dev/null 2>&1 || {
-	# trying gnu-sed's "g-version" on macos
-		if ! which gsed > /dev/null; then
-			local err_msg="${C_RED}ERROR:${C_NC} while testing ${C_BOLD}'$OSTYPE'${C_NC}:\n"
-			err_msg+="\tGNU tooling couldn't be found.\n"
-			err_msg+="\tYou should install ${C_BOLD}'brew install gnu-sed'${C_NC} and follow instructions to properly set '\$PATH' environment variable."
-			echo -e "${err_msg}"
-			error_dependencies=true
-		# non-recoverable error: exit immediately
-			exit $err_rc
-		else
-			local err_msg="${C_RED}ERROR:${C_NC} while testing ${C_BOLD}'$OSTYPE'${C_NC}:\n"
-			err_msg+="\tit seems ${C_BOLD}'brew gnu-sed'${C_NC} is installed but unconfigured.\n"
-			err_msg+="\tyou need to propely set your '\$PATH' environment variable."
-			echo -e "${err_msg}"
-			error_dependencies=true
-		# non-recoverable error: exit immediately
-			exit $err_rc
-		fi
-	}
-	# grep
+		}
+	done
+# grep
 	echo "HELLO!" | grep --perl-regex --only-matching "^HELLO!" > /dev/null 2>&1 || {
 	# trying grep' "g-version" on macos
 		if ! which ggrep > /dev/null; then
 			local err_msg="${C_RED}ERROR:${C_NC} while testing ${C_BOLD}'$OSTYPE'${C_NC}:\n"
 			err_msg+="\tGNU tooling couldn't be found.\n"
 			err_msg+="\tYou should install ${C_BOLD}'brew install grep'${C_NC} and follow instructions to properly set '\$PATH' environment variable."
-			echo -e "${err_msg}"
-			error_dependencies=true
-		# non-recoverable error: exit immediately
-			exit $err_rc
 		else
 			local err_msg="${C_RED}ERROR:${C_NC} while testing ${C_BOLD}'$OSTYPE'${C_NC}:\n"
 			err_msg+="\tit seems ${C_BOLD}'brew grep'${C_NC} is installed but unconfigured.\n"
 			err_msg+="\tyou need to propely set your '\$PATH' environment variable."
-			echo -e "${err_msg}"
-			error_dependencies=true
-		# non-recoverable error: exit immediately
-			exit $err_rc
 		fi
+		echo -e "${err_msg}"
+		error_dependencies=true
 	}
 }
 
